3fda2340a7dab59444f7fe66d233c54eee7f1c12
saroko-dnd <isaroko@mail.ru>
2018-02-21 10:30:23 +0200

    function was successfully implemented

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = (preferences = []) => {
+    let secondLoverNumber;
+
+    return preferences.reduce((sumOfTriangles, loverNumber, index) => {
+        secondLoverNumber = preferences[loverNumber - 1];
+
+        if (
+            loverNumber > index + 1 &&
+            secondLoverNumber > index + 1 &&
+            preferences[secondLoverNumber - 1] === index + 1
+        ) {
+            return sumOfTriangles + 1;
+        }
+
+        return sumOfTriangles;
+    }, 0);
 };
